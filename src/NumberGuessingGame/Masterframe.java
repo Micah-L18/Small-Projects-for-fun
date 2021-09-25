34ce31c88a088b61eb5f906607022ff7111195de
Micah-L18 <76026037+Micah-L18@users.noreply.github.com>
2021-09-25 04:00:48 -0700

    guessing game complete #5
    FINISHED!
    
    play the guessing number game :)
    small ui but for a game like this its all you need
    
    might consider making it display the results of the guess on the panel instead of console

--- src/NumberGuessingGame/Masterframe.java
+++ src/NumberGuessingGame/Masterframe.java
@@ -3,7 +3,6 @@ package NumberGuessingGame;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Random;
 
 public class Masterframe {
 
@@ -13,7 +12,7 @@ public class Masterframe {
 
         JFrame frame = new JFrame();
             frame.setVisible(true);
-            frame.setSize(500,500);
+            frame.setSize(225,250);
             frame.setResizable(false);
             frame.setTitle("Number Guessing Game!!!");
             frame.setLayout(null);
@@ -32,7 +31,7 @@ public class Masterframe {
 
         JButton play = new JButton();
             play.setVisible(true);
-            play.setBounds(225,225,50,50);
+            play.setBounds(44,100,120,50);
             play.setText("Play");
             StartPanel.add(play);
                 play.addActionListener(new ActionListener(){
@@ -41,32 +40,32 @@ public class Masterframe {
                         StartPanel.setVisible(false);
                         GamePanel.setVisible(true);
                         random_int = (int)Math.floor(Math.random()*(999-1+1)+1);
-                        System.out.println("pressed play"+random_int);
+
                     }
+
                 });
 
         JLabel test = new JLabel();
             test.setVisible(true);
-            test.setBounds(10,10,150,50);
+            test.setBounds(50,10,150,50);
             test.setText("Lets Try and Guess");
             GamePanel.add(test);
 
-
         JTextArea guess = new JTextArea();
             guess.setVisible(true);
-            guess.setBounds(225,200,100,30);
+            guess.setBounds(50,50,100,30);
             GamePanel.add(guess);
 
         JButton newgame = new JButton();
-        newgame.setVisible(true);
-        newgame.setText("play");
-        newgame.setBounds(1,350,120,50);
+            newgame.setVisible(true);
+            newgame.setText("play");
+            newgame.setBounds(45,100,120,50);
             GamePanel.add(newgame);
         newgame.addActionListener(new ActionListener(){
                 public void actionPerformed(ActionEvent e){
 
                     random_int = (int)Math.floor(Math.random()*(999-1+1)+1);
-                    System.out.println("pressed play in game"+random_int);
+
                 }
 
         });
@@ -74,7 +73,7 @@ public class Masterframe {
         JButton check = new JButton();
             check.setVisible(true);
             check.setText("Check");
-            check.setBounds(350,350,120,50);
+            check.setBounds(45,150,120,50);
             GamePanel.add(check);
             check.addActionListener(new ActionListener(){
                 public void actionPerformed(ActionEvent e){
